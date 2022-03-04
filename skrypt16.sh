46edd758d13722194489245c5b4c1a93d05ce580
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-04 19:54:24 +0100

    Skrypt16 - pokazuje pliki z katalogu zadanego jako parametr wywołania (a także podkatalogi), wynik będzie wylistowany, zmienna GLEBOKOSC kontroluje liczbe zagnieżdżeń

--- skrypt16.sh
+++ skrypt16.sh
@@ -13,3 +13,30 @@ then
 	exit 1
 fi
 
+if [ ! -d $1 ]
+then 
+	echo "$1 nie jest katalogiem! "
+	exit 1
+fi
+
+function pokaz_zawartosc_kat
+{
+	ZAWARTOSC=`ls $1`
+	for plik in $ZAWARTOSC
+	do
+		if [-d $1/$plik ]
+		then
+			if [[ $GLEBOKOSC -gt 0 ]]
+			then 
+				let GLEBOKOSC=GLEBOKOSC-1
+				echo "$1/$plik"
+				pokaz_zawartosc_kat $1/$plik
+				let GLEBOKOSC=GLEBOKOSC+1
+			fi
+		else
+			echo "$1/$plik"
+		fi 
+	done
+}
+
+pokaz_zawartosc_kat $1
