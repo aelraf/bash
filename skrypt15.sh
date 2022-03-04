ad82647894b6ccd6b28fbd7aac91753f146737de
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-04 19:44:23 +0100

    skrypt 15 - przenosi pliki z prawem wykonywania z katalogu źródłowego do docelowego, podanych jako parametry wywołania

--- skrypt15.sh
+++ skrypt15.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# skrypt przenosi pliki z prawem wykonania z katalogów źródłowego do docelowego, których nazwy są podane jako parametry wywołania
+# skrypt przenosi pliki z prawem wykonywania z katalogów źródłowego do docelowego, których nazwy są podane jako parametry wywołania
 # pozostałe pliki zostają bez zmian
 
 if [ $# -ne 2 ]
@@ -15,10 +15,17 @@ then
 	cd $1
 	for plik in *
 	do
-	
-	
-	
+		if [ -d $plik ]
+		then
+			continue
+		else
+			if [ -x $plik ]
+			then
+				mv $plik $biezacy_katalog/$2
+			fi 
+		fi 
 	done
+	cd $biezacy_katalog
 else 
 	echo "$1 lub $2 nie jest katalogiem!"
 	exit 1
