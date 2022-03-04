e9176cd3c72914d5fa5b272071dce34d7f98a254
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-04 19:56:31 +0100

    skrypt17 - początek

---
+++ skrypt17.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# skrypt usuwa wszystkie pliki o zerowej wartości (puste) z katalogu zadanym jako parametr wywołania.
+# tworzy w zadanym pliku listę nazw skasowanych plików
+#
+# nie analizuje linków symbolicznych
