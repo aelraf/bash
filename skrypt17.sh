6f2594cb82a41506c78a0beaf36ae764604d9552
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-04 20:04:57 +0100

    Skrypt17 - usuwa wszystkie puste pliki z katalogu zadanym jako pierwszy parametr wywołania, nazwy usuniętych zapisuje w pliku podanym jako drugi parametr wywołania

--- skrypt17.sh
+++ skrypt17.sh
@@ -3,3 +3,29 @@
 # tworzy w zadanym pliku listę nazw skasowanych plików
 #
 # nie analizuje linków symbolicznych
+
+if [ $# -ne 2 ]
+then
+	echo "Niepoprawna liczba argumentów."
+	echo "Użycie: $0 katalog plik_wynikowy"
+	exit 1
+fi 
+
+if [ ! -d $1 ]
+then 
+	echo "$1 nie jest katalogiem!"
+	exit 1 
+fi
+
+biezacy_katalog=`pwd`
+cd $1
+for plik in *
+do
+	if [[ ! -s $plik ]] && [[ -f $plik ]] && [[ ! -L $plik ]]
+	then
+		echo "Usunąłem plik $plik." >> $biezacy_katalog/$2
+		rm $plik -f 
+	fi 
+done
+
+cd $biezacy_katalog
