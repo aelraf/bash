8d1e359378e7b28afc3bb5c3517c132efb63d703
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-08 21:09:42 +0100

    Skrypt19 - porównuje zawartość katalogu z listą plików, oba podane jako argumenty wywołania, zwraca listę plików brakujących w katalogu oraz nieobecnych na liście, czyli sumę różnicy tych dwóch argumentów

--- skrypt19.sh
+++ skrypt19.sh
@@ -25,4 +25,35 @@ WYKAZ1=`ls$1`
 WYKAZ2=`cat $2`
 
 echo "Pliki z listy, których nie ma w katalogu:"
+for plik1 in $WYKAZ2
+do
+	ZNALEZIONO=0
+	for plik2 in $WYKAZ1
+	do 
+		if [[ $plik1 = $plik2 ]]
+		then
+			ZNALEZIONO=1
+		fi 
+	done 
+	if [[ ZNALEZIONO -eq 0 ]]
+	then
+		echo $plik1 
+	fi 
+done
 
+echo "Pliki z katalogu, których nie ma na liście: "
+for plik1 in $WYKAZ1
+do 
+	ZNALEZIONO=0
+	for plik2 in $WYKAZ2
+	do 
+		if [[ $plik1 = $plik2 ]]
+		then
+			ZNALEZIONO=1
+		fi 
+	done 
+	if [[ ZNALEZIONO -eq 0 ]]
+	then
+		echo $plik1 
+	fi 
+done 
