0d0c939e810f0502fe9fad085b3b4bac2093b93c
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-08 21:04:15 +0100

    skrypt19 - sprawdzenie poprawności parametrów wywołania

--- skrypt19.sh
+++ skrypt19.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 # porównuje zawartość katalogu z listą plików, oba podane jako argumenty wywołania
 # zwraca listę plików brakujących w katalogu i nieobecnych na liście
+
+if [ $# -ne 2 ]
+then
+	echo "Niepoprawna liczba argumentów."
+	echo "Użycie: $0 katalog_źródłowy plik_z_listą"
+	exit 1
+fi
+
+if [[ ! -d $1 ]]
+then
+	echo "$1 nie jest katalogiem!"
+	exit 1
+fi 
+
+if [[ ! -f $2 ]]
+then
+	echo "Nie ma pliku $2!"
+	exit 1
+fi 
+
+WYKAZ1=`ls$1`
+WYKAZ2=`cat $2`
+
+echo "Pliki z listy, których nie ma w katalogu:"
+
