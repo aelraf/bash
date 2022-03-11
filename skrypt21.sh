014471a6eaa8ab435647ac2dd85b1278b52292eb
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-11 21:25:20 +0100

    skrypt21 - początek

---
+++ skrypt21.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# skrypt wysyła pocztę elektroniczną do wszystkich użytkowników systemu
+# treść jest zadana zawartością pliku, będącego parametrem wywołania skryptu 
+# lista lokalnnych użytkowników jest uzyskiwana w najprostrzy sposób - przez listowanie /var/spool/mail
