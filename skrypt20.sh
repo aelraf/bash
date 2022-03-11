5170de4e970125b326afa1035fd7257ddb2e861a
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-11 21:22:23 +0100

    skrypt20 - usuwa wszystkie podkatalogi z katalogu podanego jako parametr wywołania, ich zawartość jest przeniesiona dokatalogu nadrzędnego

--- skrypt20.sh
+++ skrypt20.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
-# 
+# skrypt usuwa wszsytkie podkatalogi pierwszego parametru wywołania, ich zawartość jest przenoszona do katalogu nadrzędnego
+# usuwanie dotyczy tylko jednego poziomu podkatalogów
+
+if [ $# -ne 1 ]
+then
+	echo "Niepoprawna liczba argumentów."
+	echo "Użycie: $0 katalog"
+	exit 1
+fi
+
+if [[ ! -d $1 ]] 
+then 
+	echo "$1 nie jest katalogiem!"
+	exit 1
+fi
+
+WYKAZ=`ls $1`
+
+for plik in $WYKAZ
+do
+	if [[ -d $plik ]]
+	then
+		mv $1/$plik/* $1
+		rmdir $1/$plik
+	fi 
+done
