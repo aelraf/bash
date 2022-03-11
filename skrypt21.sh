d6ccfc51ad62becc993d40026c41c84476a8c6e2
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-11 21:29:22 +0100

    skrypt wysyła pocztę elektroniczną do wszystkich użytkowników systemu, treść jest zadana zawartością pliku- pierwszego parametru wywołania

--- skrypt21.sh
+++ skrypt21.sh
@@ -2,3 +2,23 @@
 # skrypt wysyła pocztę elektroniczną do wszystkich użytkowników systemu
 # treść jest zadana zawartością pliku, będącego parametrem wywołania skryptu 
 # lista lokalnnych użytkowników jest uzyskiwana w najprostrzy sposób - przez listowanie /var/spool/mail
+
+if [ $# -ne 1 ]
+then 
+	echo "Niepoprawna liczba argumentów."
+	echo "Użycie: $0 plik_z_wiadomoscia"
+	exit 1
+fi
+
+if [[ ! -e $1 ]]
+then 
+	echo "Nie ma pliku $1"
+	exit 1
+fi
+
+WYKAZ=`ls /var/spool/mail`
+
+for adresat in $WYKAZ
+do 
+	cat $1 | mail
+done
